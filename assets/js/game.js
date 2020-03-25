05fd60819e4d5f8f6d4010657b4e28d97e7d6b55
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-03-24 17:10:22 -0700

    testing JavaScript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert! JavaScript is running!");
+//window.alert("This is an alert! JavaScript is running!");
+
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around playerName
+// window.alert(playerName);
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+//this will do the math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  //log name test
+  var name = "Izabela";
+  console.log(name);
+
+
+  
+  //fight();
